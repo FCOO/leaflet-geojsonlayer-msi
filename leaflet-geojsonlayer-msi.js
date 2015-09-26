40711770c83a64b10c65dfd6381fe1591d0d67a7
Jesper Baasch-Larsen <jla@fcoo.dk>
2015-09-26 02:26:15 +0200

    dynamic timezones

--- leaflet-geojsonlayer-msi.js
+++ leaflet-geojsonlayer-msi.js
@@ -2,65 +2,42 @@
     /* global L */
     'use strict';
 
-		var msiDivIcon = L.divIcon({className: 'msi-marker', iconSize:[20,20], iconAnchor:[11,11]});
+    var msiDivIcon = L.divIcon({className: 'msi-marker', iconSize:[20,20], iconAnchor:[11,11]});
 
-		var latlngFormat = new LatLngFormat( 1 ); //1=Degrees Decimal minutes: N65°30.258'
+    var latlngFormat = new LatLngFormat( 1 ); //1=Degrees Decimal minutes: N65°30.258'
 
-		var dateAsHTML = function( date, language ){
-			var dateFormat = 'DD-MMM-YY HH:mm',
-					m = moment.utc(date),
-					localTxt = language == 'da' ? 'lokal' : 'local';
-			return m.local().format(dateFormat)+ '&nbsp;('+localTxt+')&nbsp;&nbsp;'	+'<em>'+m.utc().format(dateFormat) + '&nbsp;(UTC)</em>';	
-		};
+    var dateAsHTML = function( date, language, tz ){
+      var dateFormat = 'DD-MMM-YY HH:mm',
+          m = moment.utc(date),
+          localTxt = language == 'da' ? 'lokal' : 'local',
+          result;
+      if (tz == 'local') {
+          result = m.local().format(dateFormat)+ '&nbsp;('+localTxt+')';
+      } else {
+          result = m.utc().format(dateFormat) + '&nbsp;(UTC)';
+      }
+      return result;
+    };
 
 
-		L.GeoJSON.MSI = L.GeoJSON.extend({
+    L.GeoJSON.MSI = L.GeoJSON.extend({
         options: {
             language: 'en',
-						protocol: location.protocol,
-            baseurl	: '//app.fcoo.dk/warnings/msi/msi_{language}.json',
-            onEachFeature: function (feature, layer) { 
-                var point_template = '<p>Position: {latitude}&nbsp;&nbsp;{longitude}</p>';
-                if (feature.properties.language == 'da') {
-                    var popup_template = '<div class="msi"><h4>Aktuelle advarsler</h4><p>{body}</p><hr/><p>Tid: {updated}</p><hr/><p>Hovedområde: {mainarea}</p><p>Underområde: {subarea}</p><hr/>{points}<hr/><p>Kilde: <a target="_new" href="http://www.soefartsstyrelsen.dk">Søfartsstyrelsen</a></p></div>';
-                    //OLD format: var point_template = '<p>Længdegrad: {longitude}</p><p>Breddegrad: {latitude}</p>';
-                } else {
-                    var popup_template = '<div class="msi"><h4>Maritime Safety Information</h4><p>{body}</p><hr/><p>Time: {updated}</p><hr/><p>Main area: {mainarea}</p><p>Subarea: {subarea}</p><hr/>{points}<hr/><p>Source: <a target="_new" href="http://dma.dk">Danish Maritime Authority</a></p></div>';
-                    //OLD format: var point_template = '<p>Longitude: {longitude}</p><p>Latitude: {latitude}</p>';
-                }
-                var innerhtml = popup_template.replace('{title}', feature.properties.encText);
-                innerhtml = innerhtml.replace('{body}', feature.properties.navWarning);
-                innerhtml = innerhtml.replace('{updated}', dateAsHTML(feature.properties.updated, feature.properties.language));
-                innerhtml = innerhtml.replace('{mainarea}', feature.properties.mainarea);
-                innerhtml = innerhtml.replace('{subarea}', feature.properties.subarea);
-                var points = '';
-                if (feature.geometry.type !== 'Point') {
-                    for (var kk in feature.geometry.geometries[0].coordinates) {
-                        var point = feature.geometry.geometries[0].coordinates[kk];
-                        points += point_template;
-                        points = points.replace('{longitude}', latlngFormat.asTextLng(point[0]));
-                        points = points.replace('{latitude}', latlngFormat.asTextLat(point[1]));
-                    }
-                } else {
-                    points = point_template.replace('{longitude}', latlngFormat.asTextLng( feature.geometry.coordinates[0] )) ;
-                    points = points.replace('{latitude}', latlngFormat.asTextLat(feature.geometry.coordinates[1] ));
-                };
-                innerhtml = innerhtml.replace('{points}', points);
-                layer.bindPopup(innerhtml, {maxWidth: 300, maxHeight: 400});
-            },
+            timezone: 'local',
+            protocol: location.protocol,
+            baseurl : '//app.fcoo.dk/warnings/msi/msi_{language}.json',
             pointToLayer: function (feature, latlng) {  
-							var result = L.marker(latlng, {icon: msiDivIcon});
-							result.on('add', function(){ this._icon.title = 'MSI: ' + feature.properties.encText + '\n' + feature.properties.mainarea + ' - ' + feature.properties.subarea; }, result);
-							return result;
-						},
-
-						style: function (feature) { 
+                 var result = L.marker(latlng, {icon: msiDivIcon});
+                 result.on('add', function(){ this._icon.title = 'MSI: ' + feature.properties.encText + '\n' + feature.properties.mainarea + ' - ' + feature.properties.subarea; }, result);
+                 return result;
+            },
+            style: function (feature) { 
                 return {
                     weight: 2,
                     color: "#e2007a",
                     opacity: 1,
                     fillColor: "#e2007a",
-	                  fillOpacity: 0.2,
+                    fillOpacity: 0.2,
                 };
             }
 
@@ -71,16 +48,55 @@
             L.setOptions(this, options);
             this._layers = {};
             this.options.url = this.options.protocol  + this.options.baseurl.replace('{language}', this.options.language);
+
+            // Set method to perform on each feature
+            this.options.onEachFeature = function (feature, layer) { 
+                // Use click handler for substituting timezone information
+                layer.on({
+                    click: function (evt) {
+                        var point_template = '<p>Position: {latitude}&nbsp;&nbsp;{longitude}</p>';
+                        if (feature.properties.language == 'da') {
+                            var popup_template = '<div class="msi"><h4>Aktuelle advarsler</h4><p>{body}</p><hr/><p>Tid: {updated}</p><hr/><p>Hovedområde: {mainarea}</p><p>Underområde: {subarea}</p><hr/>{points}<hr/><p>Kilde: <a target="_new" href="http://www.soefartsstyrelsen.dk">Søfartsstyrelsen</a></p></div>';
+                        } else {
+                            var popup_template = '<div class="msi"><h4>Maritime Safety Information</h4><p>{body}</p><hr/><p>Time: {updated}</p><hr/><p>Main area: {mainarea}</p><p>Subarea: {subarea}</p><hr/>{points}<hr/><p>Source: <a target="_new" href="http://dma.dk">Danish Maritime Authority</a></p></div>';
+                        }
+                        var innerhtml = popup_template.replace('{title}', feature.properties.encText);
+                        innerhtml = innerhtml.replace('{body}', feature.properties.navWarning);
+                        innerhtml = innerhtml.replace('{updated}', dateAsHTML(feature.properties.updated, feature.properties.language, that.options.timezone));
+                        innerhtml = innerhtml.replace('{mainarea}', feature.properties.mainarea);
+                        innerhtml = innerhtml.replace('{subarea}', feature.properties.subarea);
+                        var points = '';
+                        if (feature.geometry.type !== 'Point') {
+                            for (var kk in feature.geometry.geometries[0].coordinates) {
+                                var point = feature.geometry.geometries[0].coordinates[kk];
+                                points += point_template;
+                                points = points.replace('{longitude}', latlngFormat.asTextLng(point[0]));
+                                points = points.replace('{latitude}', latlngFormat.asTextLat(point[1]));
+                            }
+                        } else {
+                            points = point_template.replace('{longitude}', latlngFormat.asTextLng( feature.geometry.coordinates[0] )) ;
+                            points = points.replace('{latitude}', latlngFormat.asTextLat(feature.geometry.coordinates[1] ));
+                        };
+                        innerhtml = innerhtml.replace('{points}', points);
+                        layer._map.openPopup(innerhtml, evt.latlng, {maxWidth: 300, maxHeight: 400});
+                    }
+                });
+            };
         },
 
         onAdd: function (map) {
             var that = this;
-						$.getJSON(this.options.url, function (data) {
+            $.getJSON(this.options.url, function (data) {
                 that.addData(data);
                 L.GeoJSON.prototype.onAdd.call(that, map);
             });
+            // Whenever the timezone is changed we will change the internal timezone
+            map.on("timezonechange", function(data) {
+                that.options.timezone = data.timezone;
+            });
 
-				},
+        },
+
   });
 
   return L.GeoJSON.MSI;
